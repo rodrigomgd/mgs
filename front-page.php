d98cdd214ac6d88d943472394184b9dfe2bbfcb6
Rodrigo Mendes Rosa <rodrigo.m.rosa@hotmail.com>
2018-06-11 13:00:36 -0300

    Finalização do header da pagina inicial

--- front-page.php
+++ front-page.php
@@ -12,20 +12,26 @@
 </head>
 
 <body>
-<section class="container-fluid">
-    <div id="particles-js"></div>
-    <div class="container-fluid p-5 white-text">
-        <div class="row pt-5">
-            <div class="col-md-6 p-5">
-                <img src="<?= get_theme_file_uri('assets/img/logo.png'); ?>" class="img-fluid pt-5" alt="Logo Megusta Digital">
-            </div>
-            <div class="col-md-4 ">
-                <span>social-links</span> <hr class="hr-50" > <ul class="list-inline">
-                    <li class= "list-inline-item"><a href="#"><img src="<?= get_theme_file_uri('assets/img/facebook.png') ?>" alt=""></a></li>
-                    <li class= "list-inline-item"><a href="#"><img src="<?= get_theme_file_uri('assets/img/instagram.png') ?>" alt=""></a></li>
-                </ul>
-            </div>
+<div class="container-fluid pt-5 white-text position-relative">
+<div id="particles-js"></div>
+    <div class="row pb-5 justify-content-around">
+        <div class="col-md-6 p-5">
+            <a href="<?php echo site_url() ?>"> <img src="<?= get_theme_file_uri('assets/img/logo.png'); ?>" class="img-fluid pt-5" alt="Logo Megusta Digital"></a>
         </div>
+        <div class="header-social-links">
+            <span>social-links</span> 
+            <hr class="hr-50" > 
+            <ul class="list-inline">
+                <li class= "list-inline-item"><a href="#"><img src="<?= get_theme_file_uri('assets/img/facebook.png') ?>" alt=""></a></li>
+                <li class= "list-inline-item"><a href="#"><img src="<?= get_theme_file_uri('assets/img/instagram.png') ?>" alt=""></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<section class="container pt-5">
+    <div class="row text-white text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, non doloremque asperiores quam obcaecati debitis doloribus. Perspiciatis unde voluptate ex fuga doloremque, omnis labore ipsum? Animi dicta accusamus illum nulla.
     </div>
 </section>
 <script src="<?= get_theme_file_uri('assets/js/particles.min.js') ?>"/></script>
