438e23efac8084b8349a0a39f1d26ae567716bff
rodrigomgd <rodrigo@megustadigital.com.br>
2018-07-23 12:35:58 -0300

    correções no layout

--- dist/mail.php
+++ dist/mail.php
@@ -20,10 +20,11 @@ $email_subject = "Formulário de contato do site. Cliente: ".$name.".";
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'rodrigo@megustadigital.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'digital@megusta.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to. // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_body = "<b>Detalhes:</b> <br> <b>Nome:</b> ".$name."<br> <b>Email:</b> ".$email_address." <br> <b>Mensagem:</b> ".$message;
 
 $headers .= "Content-Type: text/html; charset=UTF-8 \r\n";
+$headers .= "Cc: rodrigo@megustadigital.com.br \r\n";
 $headers .= "From: naoresponda@megusta.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 @mail($to,$email_subject,$email_body,$headers);
 echo json_encode(array(
--- dist/single.php
+++ dist/single.php
@@ -4,7 +4,7 @@
         <p class="text-center f-medium p-3" id="breadcrumbs">','</p>
         ');
     }
-?> <section class="container-fluid mt-5"><div class="row justify-content-center"><div class="col-xl-6 col-lg-7 col-md-8 col-10"> <?php if(have_posts()): while(have_posts()): the_post(); ?> <article <?php post_class("row pl-3 posts d-block"); ?> > <?php if(has_post_thumbnail()) { ?> <a class="d-block mb-3" href=" <?php echo get_permalink(); ?>"> <?php the_post_thumbnail( 'large', array("class" => "img-fluid m-auto d-block") ); ?> </a> <?php } else {
+?> <section class="container-fluid mt-5"><div class="row justify-content-center"><div class="col-xl-6 col-lg-7 col-md-8 col-10 w-1570"> <?php if(have_posts()): while(have_posts()): the_post(); ?> <article <?php post_class("row posts d-block"); ?> > <?php if(has_post_thumbnail()) { ?> <a class="d-block mb-3" href=" <?php echo get_permalink(); ?>"> <?php the_post_thumbnail( 'large', array("class" => "img-fluid m-auto d-block") ); ?> </a> <?php } else {
                     echo "Sem imagem."; 
                 } ?> <a class="f-bold posts-link" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a> <?php the_category(); ?> <p class="text-muted f-medium"><?php the_author(); echo ", "; the_date('M Y'); ?></p><div class="f-regular text-justify"> <?php the_content(); ?> </div></article><div class="f-light text-center posts-link pb-3"> <?php the_post_navigation(array(
                     'screen_reader_text' => "Leia mais:"
